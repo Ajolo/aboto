6b4ee98e6248664007674f455cd60516c16855b5
Alex Lopez <alexjlopez96@gmail.com>
2019-08-03 14:19:30 -0700

    migrate play command to command controller

--- aboto.js
+++ aboto.js
@@ -2,7 +2,6 @@
 //  GLOBALS
 //
 const Discord = require('discord.js')
-const ytdl = require('ytdl-core')
 const fs = require('fs') // fs is Node's native file system module
 const tools = require('./tools.js')
 const token = tools.token   // grab token from tools
@@ -124,62 +123,8 @@ bot.on('message', msg => {
         bulkDelete(args[0])
     }
     */
-
-    if (command === 'play') {
-        if (args.length == 0) {
-            msg.channel.send("Specify a name to search -- ie. ?play despacito")
-            return
-        }
-
-        getSong(msg, args[0])
-    }
-
+   
 })
-
-//
-// PLAY REQ HANDLING 
-//
-async function getSong(msg, songName) {
-    const voiceChannel = msg.member.voiceChannel
-
-    // verify user in voice channel
-    if (!voiceChannel) { 
-        msg.channel.send("Join a voice channel to request playback")
-        return
-    }
-
-    const songInfo = await ytdl.getInfo(songName)
-    const song = {
-        title: songInfo.title,
-        url: songInfo.video_url
-    }
-
-    // msg.channel.send(song.title)
-    try {
-        var connection = await voiceChannel.join()
-        console.log("Playing " + song.title)
-        const dispatcher = connection.playStream(ytdl(song.url, { filter : 'audioonly' }))
-        
-        dispatcher.on('end', () => {
-                console.log('Stream finished')
-                // voiceChannel.leave()
-
-                // shift the queue
-                // serverQueue.songs.shift()
-        })
-        dispatcher.on('error', err => {
-                console.log(err)
-        })
-
-        dispatcher.setVolumeLogarithmic(5 / 5);
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-
-
 //
 // LOGIN ON START
 //
---
+++ commands/play.js
@@ -0,0 +1,55 @@
+module.exports = {
+	name: 'play',
+	description: 'Play song from specified yt url!',
+	execute(msg, args) {
+        // imports
+        const ytdl = require('ytdl-core')
+
+        if (args.length == 0) {
+            msg.channel.send("Specify a name to search -- ie. ?play despacito")
+            return
+        }
+        else {
+            playSong(args[0])
+        }
+
+        async function playSong(msgUrl) {
+            const voiceChannel = msg.member.voiceChannel
+        
+            // verify user in voice channel
+            if (!voiceChannel) { 
+                msg.channel.send("Join a voice channel to request playback")
+                return
+            }
+        
+            const songInfo = await ytdl.getInfo(msgUrl)
+            const song = {
+                title: songInfo.title,
+                url: songInfo.video_url
+            }
+        
+            // msg.channel.send(song.title)
+            try {
+                var connection = await voiceChannel.join()
+                console.log("Playing " + song.title)
+                const dispatcher = connection.playStream(ytdl(song.url, { filter : 'audioonly' }))
+                
+                dispatcher.on('end', () => {
+                        console.log('Stream finished')
+                        // voiceChannel.leave()
+        
+                        // shift the queue
+                        // serverQueue.songs.shift()
+                })
+                dispatcher.on('error', err => {
+                        console.log(err)
+                })
+        
+                dispatcher.setVolumeLogarithmic(5 / 5);
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+    },
+};
